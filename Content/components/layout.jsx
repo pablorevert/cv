8c44ab83089b6fcc5b72f4fa3acb925992bd4a95
Pablo A. Revert <pablorevert@laincosoftware.com>
2020-09-15 09:35:00 -0300

    Corrijo el bug de ruteo

--- Content/components/layout.jsx
+++ Content/components/layout.jsx
@@ -1,24 +1,36 @@
 ﻿import React from 'react';
 import HeaderComponent from './header.jsx';
-import { Router, Route, Switch, hasHistory } from "react-router-dom";
+import { Link, BrowserRouter, Route, Switch, StaticRouter, Redirect } from "react-router-dom";
 import CVListComponent from './cv-list.jsx'
 import IndexLayout from './index.jsx'
 
 
 export default class LayoutComponent extends React.Component {
     render() {
-        return (
+        const app = (
             <div className="container">
-                <HeaderComponent />                
+                <HeaderComponent />
                 <div className="content">
-                        <Switch>
-                            <Route path="/cvList" component={CVListComponent} />
-                            <Route path="/" component={IndexLayout} />
-                        </Switch>
+                    <Switch>
+                        <Route path="/cvList" component={CVListComponent} />
+                        <Route path="/" component={IndexLayout} />
+                    </Switch>
                 </div>
             </div>
         );
+
+        if (typeof window === 'undefined') {
+            return (
+                <StaticRouter
+                    context={this.props.context}
+                    location={this.props.location}
+                >
+                    {app}
+                </StaticRouter>
+            );
+        }
+        return <BrowserRouter>{app}</BrowserRouter>;
     }
 
-    
+
 }
