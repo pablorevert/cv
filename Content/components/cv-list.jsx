d32748c62fc02716e9a0f6bb680a5d70d20da0f4
Pablo A. Revert <pablorevert@laincosoftware.com>
2020-09-15 08:58:48 -0300

    Revert "Data to components"
    This reverts commit 63322f690cf76bd20858af7188bda7bb07831854.

--- Content/components/cv-list.jsx
+++ Content/components/cv-list.jsx
@@ -4,42 +4,31 @@ export default class CVListComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = { data: [] };
+       // this.handleEditBtn = this.handleEditBtn.bind(this);
 
         this.state.data = [
             {
                 'title': 1,
                 'creationDate': '03/05/2020',
-                'updateDate': '15/08/2019',
-                'id': 24
+                'updateDate': '15/08/2019'
             },
             {
                 'title': 2,
                 'creationDate': '15/06/2020',
-                'updateDate': '20/08/2019',
-                'id': 36
+                'updateDate': '20/08/2019'
             }
         ];
     }
-
-    goToCv(id) {
-        console.log(id +'ok')
-    }
-
     render() {
         return (
             <div>
-                <h4 className="commentBox"></h4>
-                <div>
-                    {this.state.data.map((cv, i) =>
-                        <div key={i}>
-                            <div>
-                                <p>{cv.title}</p>
-                                <span>{cv.updateDate}</span>
-                            </div>
-                            <button onClick={() => this.goToCv(cv.id)}>Editar</button>
-                        </div>
-                        )}
-                </div>
+                {this.state.data.map((cv, i) =>
+                    <div key={i}>
+                        <p>{cv.title}</p>
+                        <span>{cv.updateDate}</span>
+                    </div>
+                    <button>Editar</button>
+            )}
             </div>
         );
     }
